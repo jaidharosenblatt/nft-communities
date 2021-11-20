5d3c8732c0705bf3b5c37a1a84c1e480b0b81dc3
Jaidha Rosenblatt <jrr59@duke.edu>
2021-11-20 08:17:21 -0800

    include date in scraping

--- src/scraping.js
+++ src/scraping.js
@@ -5,28 +5,41 @@ async function getHowRareProjects() {
   const { data } = await axios.get("https://howrare.is/drops");
   const $ = cheerio.load(data);
   let projects = [];
+  let releaseDate;
+  $("div.col")
+    .children()
+    .each((i, e) => {
+      // get date by finding calendar icon and going to it's parent
+      const date = $(e).find("i.far.fa-calendar-alt").parent().text().trim();
+      // skip newlines
+      if (date != "") {
+        releaseDate = convertDateString(date);
+      }
 
-  // loop throgh each table row
-  $("tr").each((i, e) => {
-    let p = [];
-    // add all text elements (not <a>)
-    $(e)
-      .find("td")
-      .each((j, e2) => {
-        p.push($(e2).text().trim());
-      });
-
-    const { discord, website, twitter } = gerUrls($, e);
-    const project = {
-      name: p[0],
-      website,
-      discord,
-      twitter,
-    };
-    if (project.name) {
-      projects.push(project);
-    }
-  });
+      // loop throgh each table row
+      $(e)
+        .find("tr")
+        .each((j, e2) => {
+          let p = [];
+          // add all text elements (not <a>)
+          $(e2)
+            .find("td")
+            .each((j, e3) => {
+              p.push($(e3).text().trim());
+            });
+          const { discord, website, twitter } = gerUrls($, e);
+          const project = {
+            name: p[0],
+            website,
+            discord,
+            twitter,
+            releaseDate,
+          };
+          if (project.name) {
+            projects.push(project);
+          }
+        });
+    });
   return projects;
 }
 
@@ -60,6 +73,33 @@ function gerUrls($, e) {
     }
   });
   return { discord, website, twitter };
+}
+
+function convertDateString(date) {
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  if (date === "No date specified yet") return undefined;
+  const [monthStr, dayStr] = date.split(" ");
+  const month = months.findIndex((str) => str === monthStr) + 1;
+  const day = parseInt(dayStr);
+  const now = new Date();
+  // year + 1 if we have already passed the specified month
+  const year =
+    month < now.getMonth() ? now.getFullYear() + 1 : now.getFullYear();
+
+  return new Date(`${month}/${day}/${year}`);
 }
 
 module.exports = { getHowRareProjects };
