fcb3296340fadfdda6988bbe796dca5c97b29cd0
Jaidha Rosenblatt <jrr59@duke.edu>
2021-11-19 19:01:39 -0800

    add cron back

--- src/cron.js
+++ src/cron.js
@@ -1,18 +1,7 @@
-const { updateFollowers } = require("./twitter");
-const Project = require("./models/project");
+const { updateAllFollowers } = require("./twitter");
 
 async function main() {
-  //   const projects = await Project.find({}).sort("updatedAt").limit(299);
-  //   let updates = 0;
-  //   await Promise.all(
-  //     projects.map(async (p) => {
-  //       const updateMade = await updateFollowers(p._id);
-  //       if (updateMade) {
-  //         updates++;
-  //       }
-  //     })
-  //   );
-  console.log(`${updates} updates made`);
+  await updateAllFollowers();
 }
 
 module.exports = main;
