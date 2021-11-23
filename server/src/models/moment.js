72cccf50dcf5547082e6350b6a0320cac616f05d
Jaidha Rosenblatt <jrr59@duke.edu>
2021-11-23 15:50:08 -0800

    trying aggregate queries for GET projects

--- server/src/models/moment.js
+++ server/src/models/moment.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
+const Project = require("./project");
 
 const momentSchema = new mongoose.Schema(
   {
     project: {
       type: mongoose.Types.ObjectId,
       required: true,
+      ref: Project,
     },
     twitterFollowers: {
       type: Number,
--- server/src/routers/projects.js
+++ server/src/routers/projects.js
@@ -2,6 +2,7 @@ const express = require("express");
 const Project = require("../models/project");
 const { updateAllFollowers, updateTweetEngagement } = require("../api/twitter");
 const { scrapeProjects } = require("../scraping/");
+const Trend = require("../models/trend");
 
 const router = new express.Router();
 
@@ -34,8 +35,44 @@ router.post("/updateTweets", async (req, res) => {
 
 router.get("/projects", async (req, res) => {
   try {
-    const projects = await Project.find({}).sort("-twitterFollowers");
-    res.send(projects);
+    const projects0 = await Project.aggregate([
+      {
+        // find trend matching this project
+        $lookup: {
+          from: "trends",
+          localField: "_id",
+          foreignField: "project",
+          as: "trends",
+        },
+      },
+      { $unwind: { path: "$trends" } }, // make trends one element
+      { $match: { "trends.timePeriod": "all" } }, // filter just for the desired timePeriod
+      {
+        $project: {
+          name: 1,
+          campId: 1,
+          articleId: 1,
+          "trends.followingPercentChange": 1,
+          "trends.followingChange": 1,
+          twitterFollowers: 1,
+          twitterUrl: 1,
+          twitterAverageTweetEngagement: 1,
+        },
+      },
+    ])
+      .sort("-trends.followingChange")
+      .limit(10);
+    // const projects = await Project.find({});
+    // const modified = await Promise.all(
+    //   projects.map(async (p) => {
+    //     const { followingChange, followingPercentChange } = await Trend.find({
+    //       project: p._id,
+    //     });
+    //     return { ...p._doc, followingPercentChange };
+    //   })
+    // );
+    // modified.sort((a, b) => a._id > b._id);
+    res.send(projects0);
   } catch (e) {
     sendError(e, res);
   }
