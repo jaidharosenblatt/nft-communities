ee0e30612a1e617e6b0b20ba3422e64449640a4d
Jaidha Rosenblatt <jrr59@duke.edu>
2021-11-29 07:44:45 -0800

    added makeshift mobile view

--- client/src/components/App.tsx
+++ client/src/components/App.tsx
@@ -6,17 +6,22 @@ import ThemeSelector from "../themes/ThemeSelector";
 import Projects from "./projects/Projects";
 import useWindowDimensions from "../hooks/useWindowDimensions";
 import { useState } from "react";
+import Footer from "./footer/Footer";
 
 function App(): JSX.Element {
   const { darkMode } = useAppSelector((state) => state.status);
-  const [showFilters, setShowFilters] = useState<boolean>(true);
+  const [showFilters, setShowFilters] = useState<boolean>(false);
 
   return (
     <ThemeSelector isDark={darkMode}>
       <div className="grid">
         <Navbar />
         {showFilters && <Filters />}
+        <div className="hide-mobile">
+          <Filters />
+        </div>
         <Projects />
+        <Footer showFilter={showFilters} onClick={() => setShowFilters(!showFilters)} />
       </div>
     </ThemeSelector>
   );
