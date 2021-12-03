74d0eb3c5d2dbc2edf6846b5003df3d263c1b77f
Jaidha Rosenblatt <jrr59@duke.edu>
2021-12-02 22:11:40 -0800

    add BS logo

--- client/src/components/navbar/Navbar.tsx
+++ client/src/components/navbar/Navbar.tsx
@@ -1,13 +1,17 @@
 import "./Navbar.css";
 import DarkModeSwitch from "../form/DarkModeSwitch";
+import BSLogo from "../../static/BSLogo.svg";
 
 export default function Navbar() {
   return (
     <div className="navbar">
       <div className="row">
-        <h1>
-          Move<span style={{ color: "var(--primary)" }}>mints</span>
-        </h1>
+        <div className="logo-row">
+          <img alt="bs-logo" src={BSLogo} />
+          <h1>
+            Move<span style={{ color: "var(--primary)" }}>mints</span>
+          </h1>
+        </div>
 
         <div className="body">
           <div className="right">
