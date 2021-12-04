b96e50a6adbd82c7e69ac7517ba67c48f519ac5d
Jaidha Rosenblatt <jrr59@duke.edu>
2021-12-04 14:52:59 -0800

    fix mobile styling css for project holders

--- client/src/components/project-card/ShowGraphButton.tsx
+++ client/src/components/project-card/ShowGraphButton.tsx
@@ -1,13 +1,14 @@
-import { Button, Skeleton } from "antd";
+import { Button } from "antd";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
 
 import { setIsVisible } from "../../redux/graph";
 export default function ShowGraphButton() {
   const dispatch = useAppDispatch();
   const isVisible = useAppSelector((state) => state.graph.isVisible);
+  const loading = useAppSelector((state) => state.status.loading);
 
   return (
-    <Button onClick={() => dispatch(setIsVisible(!isVisible))} type="primary">
+    <Button loading={loading} onClick={() => dispatch(setIsVisible(!isVisible))} type="primary">
       View Trends
     </Button>
   );
