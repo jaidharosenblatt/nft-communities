390f0e1d409096d1356cd5ced311353582ad0129
Jaidha Rosenblatt <jrr59@duke.edu>
2021-12-04 19:38:53 -0800

    Fix graph size

--- client/src/components/graph/Graph.tsx
+++ client/src/components/graph/Graph.tsx
@@ -1,11 +1,13 @@
 import "./Graph.css";
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip } from "recharts";
-import { useAppSelector } from "../../redux/hooks";
 import { Skeleton } from "antd";
+import { useAppSelector } from "../../redux/hooks";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
+import { GRAPH_WIDTH, GRAPH_WIDTH_MOBILE } from "../../constants";
 
 export default function Graph() {
   const { data, field } = useAppSelector((state) => state.graph);
-
+  const { isMobile } = useWindowDimensions();
   if (!data) {
     return <Skeleton />;
   }
@@ -31,24 +33,32 @@ export default function Graph() {
   }
 
   return (
-    <AreaChart width={450} height={250} data={data}>
-      <defs>
-        <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-          <stop offset="5%" stopColor="var(--primary)" stopOpacity={0.8} />
-          <stop offset="95%" stopColor="var(--primary)" stopOpacity={0} />
-        </linearGradient>
-      </defs>
-      <XAxis tickFormatter={formatDate} dataKey="date" />
-      <YAxis domain={[(dataMin: number) => dataMin, "auto"]} />
-      <CartesianGrid vertical={false} />
-      <Tooltip labelFormatter={formatTooltipDate} formatter={formatTooltip} />
-      <Area
-        type="monotone"
-        dataKey="value"
-        stroke="var(--primary)"
-        fillOpacity={1}
-        fill="url(#colorPv)"
-      />
-    </AreaChart>
+    <div className="graph">
+      <AreaChart
+        margin={{ top: 0, right: 0, left: 0, bottom: 0 }}
+        // use hardcoded sizing since Responsive Container doesn't work with hooks :(
+        width={isMobile ? GRAPH_WIDTH_MOBILE : GRAPH_WIDTH}
+        height={250}
+        data={data}
+      >
+        <defs>
+          <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
+            <stop offset="5%" stopColor="var(--primary)" stopOpacity={0.8} />
+            <stop offset="95%" stopColor="var(--primary)" stopOpacity={0} />
+          </linearGradient>
+        </defs>
+        <XAxis tickFormatter={formatDate} dataKey="date" />
+        <YAxis domain={[(dataMin: number) => dataMin, "auto"]} />
+        <CartesianGrid vertical={false} />
+        <Tooltip labelFormatter={formatTooltipDate} formatter={formatTooltip} />
+        <Area
+          type="monotone"
+          dataKey="value"
+          stroke="var(--primary)"
+          fillOpacity={1}
+          fill="url(#colorPv)"
+        />
+      </AreaChart>
+    </div>
   );
 }
--- client/src/components/graph/GraphCard.tsx
+++ client/src/components/graph/GraphCard.tsx
@@ -1,13 +1,16 @@
 import { Button, Modal } from "antd";
+import { GRAPH_MODAL_WIDTH, GRAPH_MODAL_WIDTH_MOBILE } from "../../constants";
+import useWindowDimensions from "../../hooks/useWindowDimensions";
 import { closeHighlightedProject } from "../../redux/actionCreators";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-import TimePeriodSelector from "../form/TimePeriodSelector";
 import ProjectCard from "../project-card/ProjectCard";
 import FieldSelector from "./FieldSelector";
 import Graph from "./Graph";
 
 export default function GraphCard() {
-  const project = useAppSelector((state) => state.graph.project);
+  const { project } = useAppSelector((state) => state.graph);
+  const { isMobile } = useWindowDimensions();
+
   const showModal = project !== undefined;
   const dispatch = useAppDispatch();
   const close = () => {
@@ -15,11 +18,16 @@ export default function GraphCard() {
   };
 
   return (
-    <Modal onCancel={close} footer={null} visible={showModal}>
+    <Modal
+      width={isMobile ? GRAPH_MODAL_WIDTH_MOBILE : GRAPH_MODAL_WIDTH}
+      onCancel={close}
+      footer={null}
+      visible={showModal}
+    >
       {project && <ProjectCard project={project} />}
       <FieldSelector />
-
       <Graph />
+
       <Button type="primary" style={{ width: "100%" }} onClick={close}>
         Close Trends
       </Button>
